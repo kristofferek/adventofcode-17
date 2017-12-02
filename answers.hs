c6b9c53696ad3ef1e7215690656b7e7a0b5d2480
Kristoffer Ek <kristofferboek@gmail.com>
2017-12-02 15:59:54 +0100

    Day 2

--- answers.hs
+++ answers.hs
@@ -1,3 +1,5 @@
+import Data.List.Split
+
 -- 1A
 sumOfAdjecents :: [Integer] -> Integer
 sumOfAdjecents l = sumOfAdjecents' (head l) l
@@ -14,3 +16,18 @@ sumOfOpposite c = (*2) $ sumOfOpposite' cStart cEnd
     sumOfOpposite' [] [] = 0
     sumOfOpposite' (s:ss) (e:es) | s == e    = s + sumOfOpposite' ss es
                                  | otherwise = sumOfOpposite' ss es
+
+-- 2A
+checkSum :: [Integer] -> Integer
+checkSum s = sum $ map rowDiff rows
+  where
+    rows = chunksOf 16 s
+    rowDiff row = maximum row - minimum row
+
+evenlyDivSum :: [Integer] -> Integer
+evenlyDivSum s = sum $ map rowDiff rows ++ map revRowDiff rows
+  where
+    rows = chunksOf 16 s
+    revRowDiff r = rowDiff (reverse r)
+    rowDiff [] = 0
+    rowDiff (x:xs) = rowDiff xs + sum [ x `div` y | y <- xs, x `mod` y == 0]
